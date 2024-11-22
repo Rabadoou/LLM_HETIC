5220a55d577539dda84ae2f7578a498454fac582
Rabadoou <onestaloklm@gmail.com>
2024-11-22 14:30:51 +0100

    test

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
+import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-DROPBOX_ACCESS_TOKEN = ("sl.CBOF6eD65Szij_9WPmNCmYyqOp2aqyzow4e_NwIvxl0HZQ5Dqsg96S1dUveoIpTrFG-aakiK-Z9MSQ4TV9_0VfV4dfoV77V_vCY0zfMLOF1UEvy2hPDjpbYGoNdk7SDSBUcSQhyduLSg")
+DROPBOX_ACCESS_TOKEN = os.getenv("DROPBOX_ACCESS_TOKEN")
 
 DEFAULT_TEMPERATURE = 37   
