74930375dfbec39431e1b86587828cf139507398
Rabadoou <onestaloklm@gmail.com>
2024-11-22 14:50:50 +0100

    variable d'environement

--- config.py
+++ config.py
@@ -5,4 +5,7 @@ load_dotenv()
 
 DROPBOX_ACCESS_TOKEN = os.getenv("DROPBOX_ACCESS_TOKEN")
 
+if DROPBOX_ACCESS_TOKEN is None:
+    raise ValueError("La variable d'environnement DROPBOX_ACCESS_TOKEN n'est pas définie.")
+
 DEFAULT_TEMPERATURE = 37   
